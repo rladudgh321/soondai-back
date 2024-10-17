a969845f1d2e0d9c541af4bd46be9672a3ab9b27
kyh <kyh_0625@naver.com>
2024-10-17 22:33:49 +0900

    aws-1

--- src/main.ts
+++ src/main.ts
@@ -52,6 +52,6 @@ async function bootstrap() {
   const port = 3065;
   console.info(`서버모드는 ${stage}이고 port는 ${port}`);
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
